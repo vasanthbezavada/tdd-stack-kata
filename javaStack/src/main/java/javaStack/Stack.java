cd9ba9cca4bf9eac93874fa81ae712660151b241
vasanthbezavada <kumarbezavada@gmail.com>
2020-08-18 00:00:39 +0530

    basic stack tdd approach

---
+++ javaStack/src/main/java/javaStack/Stack.java
@@ -0,0 +1,39 @@
+package javaStack;
+
+public class Stack {
+
+	public String[] stringStack ;
+	public int currentIndex;
+	public int size;
+	
+	public Stack(int size) {
+		this.size=size;
+		this.stringStack= new String[size];
+		this.currentIndex=0;
+	}
+	
+	public void push(String element) throws Exception {
+		if(element== null)
+			throw new Exception();
+		else {
+		stringStack[this.currentIndex]=element;
+		this.currentIndex++;	
+		}
+	}
+	
+	public String pop() {
+		if (this.currentIndex==0) {
+			return null;
+		}
+		else {
+		String picked = stringStack[this.currentIndex-1];
+		stringStack[this.currentIndex-1]=null;
+		this.currentIndex--;
+		return picked;
+		}
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ javaStack/src/test/java/javaStack/StackTest.java
@@ -0,0 +1,35 @@
+package javaStack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StackTest {
+
+	@Test
+	public void pushAndPopSingleItem() throws Exception {
+		Stack stack = new Stack(5);
+		stack.push("vasanth");
+		assertEquals("vasanth", stack.pop());		
+	}
+	@Test
+	public void pushAndPopMultiple() throws Exception {
+		Stack stack = new Stack(5);
+		stack.push("vasanth");
+		stack.push("kumar");
+		assertEquals("kumar",stack.pop());
+		assertEquals("vasanth", stack.pop());			
+	}
+	@Test
+	public void popEmptyStack() {
+		Stack Stack = new Stack(5);
+		assertEquals(null,Stack.pop());
+	}
+	@Test
+	public void pushNull() {
+		Stack stack = new Stack(10);
+		assertThrows(Exception.class,()-> stack.push(null));
+	}
+
+}
